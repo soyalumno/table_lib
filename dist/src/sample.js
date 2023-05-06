e5cd0e68a22b2018cf591bbfb73402f3306e8fc2
soyalumno <soy_alumno@yahoo.co.jp>
2023-05-06 21:03:55 +0900

    dist/src以下のファイルを追加

---
+++ dist/src/sample.js
@@ -0,0 +1,24 @@
+"use strict";
+class PrivateFunc {
+    /** Tableクラスのサンプルコード */
+    static tableSample() {
+        // シート名・主キーを指定してTableクラスを生成
+        const tbl = buildTable('main!A1:ZZ', 'ID');
+        // テーブルを全てクリアして、新規データベースをセット
+        // - 既存データがあるならセット不要
+        tbl.resetTable([{ ID: '101', foo: 'test' }, { ID: '102', foo: 'test2' }]);
+        // プロパティからアクセス
+        console.log(tbl.head);
+        console.log(tbl.hashes);
+        // 特定のデータを検索して行番号を取得
+        console.log(tbl.findRecord({ ID: '101' })?.row);
+        // データを指定して更新
+        const record = tbl.findRecord({ ID: '102' });
+        if (record) {
+            record.hash.foo = 'updated';
+            tbl.updateRecords([record]);
+        }
+        // 末尾にデータを追記
+        tbl.appendRecords([{ ID: '1004', foo: 'appended' }]);
+    }
+}
---
+++ dist/src/table.js
@@ -0,0 +1,146 @@
+"use strict";
+/** 指定した範囲のテーブルを読み書きするための抽象クラス */
+class Table {
+    /** Type Guard */
+    static isTRow(value) {
+        return (value.row !== undefined && typeof value.row === 'number');
+    }
+    /**
+     * Tableクラスコンストラクタ
+     * @param range テーブル範囲文字列
+     * @param primary_key 主キーのカラム名文字列
+     * @param rowFactory 具象Rowオブジェクト生成用処理
+     */
+    constructor(range, primary_key, rowFactory = (head, hash, row) => {
+        const primary_key = this.primary_key;
+        const row_object = {
+            head,
+            hash,
+            row: row || NaN,
+            /** Hashを見出しの順番に従った配列に変換する */
+            toValues() {
+                return this.head.map((col) => hash[col] === 'NaN' ? undefined : hash[col]);
+            },
+            /** 指定したHashが一致しているか比較 */
+            isEqual(other) {
+                return this.hash[primary_key] === other[primary_key];
+            }
+        };
+        return row_object;
+    }) {
+        /** 見出し */
+        this.head = [];
+        /** データオブジェクト */
+        this.records = [];
+        /** データ実体 */
+        this.hashes = [];
+        const [sheet, a1note] = range.split('!');
+        this.sheet = sheet;
+        const head_row = parseInt(a1note.split(':')[0].replace(/[^\d]+/, ''));
+        this.head_row = isNaN(head_row) ? 1 : head_row;
+        this.head_col = a1note.split(':')[0].replace(/(\d+)/, '');
+        this.range = range;
+        this.primary_key = primary_key;
+        this.rowFactory = rowFactory;
+        this.ss = SpreadsheetApp.getActive();
+        this.getExistRecords();
+    }
+    /** 二次元のテーブルデータを見出しをキーとしたオブジェクト配列に変換する */
+    toRecords(df) {
+        const [head, ...values_arr] = df;
+        return values_arr.map((values) => head.reduce((acc, col, i) => {
+            acc[col.toString()] = values[i]?.toString() || '';
+            return acc;
+        }, {}));
+    }
+    /** データが存在する最終行を探す */
+    lastRow() {
+        const [last_record] = this.records.slice(-1);
+        return last_record?.row || this.head_row;
+    }
+    /**
+     * 条件にマッチする先頭のRecordを返す
+     * @param target 検索条件(Hash or Row or RowNumber)
+     */
+    findRecord(target) {
+        return this.records.find((r) => Table.isTRow(target) ? r.isEqual(target.hash) : (typeof target === 'number') ?
+            r.row === target : r.isEqual(target));
+    }
+    /** 指定した範囲のデータをRow形式で取得 */
+    getExistRecords() {
+        // テーブル範囲の２次元配列を取得
+        const resp = Sheets.Spreadsheets?.Values?.batchGet(this.ss.getId(), {
+            ranges: [this.range],
+        });
+        const df = resp?.valueRanges?.[0].values || [[]];
+        // Row形式の配列を生成
+        const [head] = df;
+        const records = this.toRecords(df).map((hash, i) => this.rowFactory(head, hash, this.head_row + 1 + i));
+        this.head = head;
+        this.records = records;
+        this.hashes = records.map((r) => r.hash);
+        return { head, records };
+    }
+    /** 指定したデータでテーブルを再作成する */
+    resetTable(records) {
+        const data = records.map((r) => Table.isTRow(r) ? r.hash : r)
+            .map((hash, i) => {
+            const data = {
+                range: `${this.sheet}!${this.head_col}${this.head_row + 1 + i}`,
+                values: [this.rowFactory(this.head, hash).toValues()],
+            };
+            return data;
+        });
+        if (data.length > 0) {
+            // シートをクリア
+            Sheets.Spreadsheets?.Values?.batchClear({ ranges: [`${this.sheet}!${this.head_col}${this.head_row + 1}:ZZ`] }, this.ss.getId());
+            // シートを上書き
+            Sheets.Spreadsheets?.Values?.batchUpdate({
+                valueInputOption: 'USER_ENTERED',
+                data,
+            }, this.ss.getId());
+            // プロパティ更新
+            this.getExistRecords();
+        }
+    }
+    /** 指定したデータでシートを上書きする(一致するデータが無ければ上書きしない) */
+    updateRecords(records) {
+        // 上書き範囲の配列を生成
+        const data = records.map((r) => Table.isTRow(r) ? r.hash : r)
+            .reduce((acc, hash) => {
+            const record = this.findRecord(hash);
+            if (record) {
+                acc.push({
+                    range: `${this.sheet}!${this.head_col}${record.row}`,
+                    values: [this.rowFactory(this.head, hash, record.row).toValues()],
+                });
+            }
+            return acc;
+        }, []);
+        if (data.length > 0) {
+            // シートを上書き
+            Sheets.Spreadsheets?.Values?.batchUpdate({
+                valueInputOption: 'USER_ENTERED',
+                data,
+            }, this.ss.getId());
+            // プロパティ更新
+            this.getExistRecords();
+        }
+    }
+    /** 指定したデータをテーブルの末尾に追記する */
+    appendRecords(records) {
+        // 追記用二次元配列を生成
+        const values = records
+            .map((r) => Table.isTRow(r) ? r.hash : r)
+            .map((hash) => this.rowFactory(this.head, hash).toValues());
+        // シートの末尾に追記
+        if (values.length > 0) {
+            Sheets.Spreadsheets?.Values?.append({ values }, this.ss.getId(), `${this.sheet}!${this.head_col}${this.lastRow() + 1}`, { valueInputOption: 'USER_ENTERED' });
+            // プロパティ更新
+            this.getExistRecords();
+        }
+    }
+}
+function buildTable(range, primary_key) {
+    return new Table(range, primary_key);
+}
