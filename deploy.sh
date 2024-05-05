acfa369aa1c63a250cb370e79a4a602a943a6ebc
soyalumno <soy_alumno@yahoo.co.jp>
2024-05-05 13:00:46 +0900

    chore デプロイスクリプトを修正

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 # 最新のデプロイIDを取得
-ID=`clasp deployments | tail -1 | awk '{print $2}'`
+LATEST=$(clasp deployments | tail -1)
+ID=$(echo "$LATEST" | awk '{print $2}')
 
-if [ -n "$ID" ];then
-  clasp deploy --deploymentId $ID
+echo 'Latest deployments:'
+echo "$LATEST"
+
+if [[ -n "$ID" ]]; then
+  # 確認用のプロンプトを出力
+  read -p "Are you sure you want to deploy with ID $ID? (y/N): " CONFIRMATION
+
+  if [[ "${CONFIRMATION,,}" == "y" ]]; then
+    clasp deploy --deploymentId "$ID"
+  else
+    echo 'Deployment aborted'
+  fi
 else
-  echo 'not found deployment ID'
+  echo 'Not found deployment ID'
 fi
 
